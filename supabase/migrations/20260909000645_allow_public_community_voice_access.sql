a514aa987b32aaf5a1d23e0276791e0ce4867cc1
FONGQUANMING <quanmingfong0720@gmail.com>
2026-09-09 08:28:02 +0800

    fix cannot join voice room problem

--- supabase/migrations/20260909000645_allow_public_community_voice_access.sql
+++ supabase/migrations/20260909000645_allow_public_community_voice_access.sql
@@ -78,8 +78,7 @@ stable
 security definer
 set search_path = public, pg_temp
 as $$
-  select auth.uid() is not null
-    and input_topic ~ '^community-voice:[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$'
+  select input_topic ~ '^community-voice:[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$'
     and exists (
       select 1
       from public.communities community
